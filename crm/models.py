b47ef2335be0c3f075b22dd4d1c36a74483f8483
mhemu95 <hemu0995@gmail.com>
2022-02-23 18:09:42 +0600

    linked to postgresql

--- crm/models.py
+++ crm/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Leads(models.Model):
+    title = models.CharField(max_length=20)
+    details = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at= models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Leads'
+
+    def __str__(self):
+        return self.title + ' || ' + str(self.pk) 
